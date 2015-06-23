c158d2d12c1cbd2a33a9f70b220fe991ab4f39e6
Richard Potter <potter@axsh.net>
2015-06-23 23:25:49 +0900

    New --monitor option kind of usable now

--- vnc-through-ssh.sh
+++ vnc-through-ssh.sh
@@ -286,17 +286,43 @@ open-port-list-for-monitor()
     echo "$count"
     echo "$monitors"
 
-    while read ln; do
+    while read ln <&9 ; do
 	p1="${ln##*:}"
 	p2="${p1%% *}"
 	p3="${p2%%,*}"
 	open-one-monitor "$p3"
-    done <<<"$monitors"
+    done 9<<<"$monitors"
 }
 
 open-one-monitor()
 {
-    echo "TODO: $1"
+    ( # subshell is necessary to generate new 22 and 44 file descriptors
+	vncport="$1"
+
+	tf=/tmp/tmpfifo
+	rm -f $tf
+	mkfifo $tf
+	exec 22> >(cat >$tf)
+	exec 44< $tf
+
+	# Randomize the temporary port because MacOS's nc will queue
+	# multiple server requests to the same port, instead of
+	# failing as on Linux.  This can make the behavior very
+	# confusing when vncviewer launching fails for some reason.
+	# It also makes it difficult to test properly for a free port.
+	# This random solution still leaves a small chance an inuse
+	# port will be chosen.  The code skips the common vnc port
+	# choices (5900-5999) to reduce this chance.
+	rand=$(( $RANDOM  % 2000 ))
+	lport=$(( 5900 + 100 + rand )) 
+	[ "$localport" != "" ] && lport="$localport"
+
+	(echo "nc $localhost_ref $vncport" ; nc -l "$lport") <&44 | eval "$eval_for_shell"  >&22 &
+	if [ "$localport" == "" ]; then
+	    sleep 0.2  # sleep long enough for nc to open the listening port
+	    nc localhost "$lport"
+	fi
+    )
 }
 
 parse-parameters "$@"
