36475656cf44c830a029612114976fab55c2867e
Richard Potter <potter@axsh.net>
2016-05-06 17:58:07 +0900

    Substituted python code for readlink to make portable to macos

--- dyn.sh
+++ dyn.sh
@@ -3,7 +3,10 @@
 # ssh dyn-user@IP-pattern-flags
 # ssh dyn--p24-19999-r
 
-abspath="$(readlink -f "$0")"
+# the python code duplicates "readlink -f" functionality, but is portable to OSX.
+# from: http://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
+#   this answer-> http://stackoverflow.com/a/1115074
+abspath="$(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "$0")"
 
 if [[ "$1" == *setup ]]; then
     cat >> ~/.ssh/config <<EOF
